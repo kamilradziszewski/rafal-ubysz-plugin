cac9ebd8a6f24a56c07cdc8a46d02cf738a50443
Kamil Radziszewski <contact@kamilradziszewski.com>
2018-02-15 12:15:44 +0100

    Initial commit

---
+++ address_post_type.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Address Post Type
+ * Description: Plugin for Rafal Ubysz Ultrasonografia WordPress Theme (adds address post type)
+ * Version: 1.0
+ * Author: Kamil Radziszewski
+ * License: GPL2
+ */
+
+ /*
+ Address Post Type is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Address Post Type is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Address Post Type. If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html
+*/
